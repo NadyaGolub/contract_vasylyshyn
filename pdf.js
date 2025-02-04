752a1f73431416421d5093b3e4b6d3e64289599c
NadyaGolub <nadyagolub@ukr.net>
2025-02-04 12:23:49 +0200

    filename

--- pdf.js
+++ pdf.js
@@ -5,7 +5,7 @@ window.onload = function() {
 
     html2pdf().from(invoice).set({ // Використовуємо змінну invoice
       margin: [10, 10, 10, 10],
-      filename: 'contract_vasylyshyn.pdf',
+      filename: 'vasylyshyn.pdf',
       image: { type: 'jpeg', quality: 0.98 },
       html2canvas: {
         scale: 2.5,
